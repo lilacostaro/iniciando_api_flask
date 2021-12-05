c557fbd58b728cdcf4aad0558f84078b0b389686
lilacostaro <lila.costa.ro@gmail.com>
2021-12-05 02:10:55 -0300

    Remove linhas desnecessarias em run.py

--- run.py
+++ run.py
@@ -8,23 +8,3 @@ def ola(numero):
 
 if __name__=='__main__':
     app.run(debug=True)
-
-
-tarefas = [
-    {'id': 0,
-    'responsavel': 'Camila',
-    'tarefa': 'Lavar Louça',
-    'status': 'Incompleto'},
-    {'id': 1,
-    'responsavel': 'Karine',
-    'tarefa': 'Pagar o Cartão',
-    'status': 'Incompleto'},
-    {'id': 2,
-    'responsavel': 'Barbara',
-    'tarefa': 'Pagar o Cartão',
-    'status': 'Incompleto'},
-    {'id': 3,
-    'responsavel': 'Camila',
-    'tarefa': 'Fazer comida',
-    'status': 'Completo'}
-]
